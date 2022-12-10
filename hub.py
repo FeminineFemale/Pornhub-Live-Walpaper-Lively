61c5054377038a45448275a0bc1cabb947bcf98a
Tyler <randomtylerc@gmail.com>
2022-12-09 17:21:32 -0800

    add stuff

---
+++ hub.py
@@ -0,0 +1,126 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import json
+import random
+import time
+import threading
+from winput import *
+from yt_dlp import YoutubeDL
+
+def lcm(a, b):
+    if a > b:greater = a
+    else:greater = b
+    while(True):
+        if((greater % a == 0) and (greater % b == 0)):
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+
+def run(i):
+    reqs = requests.get(config["searchPornhubUrl"])
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    urls = []
+
+    substring = "/view_video.php?viewkey=ph"
+    for link in soup.find_all('a'):
+        if substring in str(link.get('href')):
+            (urls).append(link.get('href'))
+    #print(urls)
+    vid = random.choice(urls)
+    for ih in lim:
+        if vid == ih and config["dontPlaySameVidTwicePerCurrentRun"]:
+            raise Exception("vid has been played already")
+    lim.append(vid)
+    streamurl = f"https://www.pornhub.com{vid}"
+
+    print(streamurl)
+
+    fifs = YoutubeDL().extract_info(streamurl, download=False)
+
+    gias = fifs["resolution"].split("x")
+    tuy = lcm(int(gias[0]), int(gias[1]))
+    gias = f'{round(tuy / 1080)}:{round(tuy / 1920)}'
+
+    if str(gias) != "16:9" and config["disableNon16:9Videos"] is False:
+        raise Exception(f"not 16:9 but {gias}")
+
+
+    godshiz = fifs['formats'][4]['url']
+    #godshiz = streamurl
+    if godshiz == None or fifs['duration'] < 10 or fifs['duration'] > config["maxSecondsVideoLength"]:
+        raise Exception(f"video too long it is {fifs['duration']} and max {config['maxSecondsVideoLength']}")
+    print("m3u8 url = " + godshiz)
+    with open(f'{str(dir[i])}\\livelyinfo.json', 'r+') as f:
+        data = json.load(f)
+        data['Contact'] = godshiz
+        data['FileName'] = godshiz
+        data['Type'] = 10
+        data['Title'] = 'Pornhub monitor ' + str(i)
+        f.seek(0)
+        json.dump(data, f, indent=4)
+        f.truncate()
+    print(fifs['duration'])
+    dur[i] = (fifs['duration'])
+    print(str(dur))
+    print(f"done monitor {i}")
+    fif.append(f"https://www.pornhub.com{vid}")
+
+def d(x):
+    while True:
+
+        while True:
+            try:
+                run(x)
+                break
+            except Exception as e:
+                print(f"error on video {x} with {e}")
+        print("refreash dick")
+        print(f'{config["Lively.exeLocation4"]} setwp --file \"{dir[i]}\" --monitor {x + 1}')
+        os.system(f'{config["Lively.exeLocation4"]} setwp --file "{dir[x]}" --monitor {x + 1}')
+        time.sleep(dur[x])
+
+
+def fe():
+    hook_keyboard(keyboard_callback)
+    wait_messages()
+
+
+def keyboard_callback(event):
+    if config["keyboardDebugForInteger"]:
+        print(str(event.vkCode))
+    if str(event.vkCode).find(str(config["keybindToTurnOffInteger/NotASCII"])) != -1:
+        os.system("taskkill /f /im mpv.exe")
+        os.system("taskkill /f /im lively.exe")
+        os._exit(0)
+
+
+if __name__ == "__main__":
+    with open("config.txt") as f:
+        config = json.load(f)
+    nm = len(config["LocationOfLivelyWalpapers"])
+    config["Lively.exeLocation4"] = config["Lively.exeLocation"].replace(" ", '" "')
+
+    os.startfile(config["Lively.exeLocation"])
+
+    dir = []; fif = []; dur = {}; lim = []
+    for i in config["LocationOfLivelyWalpapers"]: dir.append(i)
+    print(dir)
+    f = threading.Thread(target=fe, args=())
+    f.start()
+
+
+    with open('log.hub.txt', 'a') as f:
+        f.write(f'{"".join(["-" for f in range(200)])}\n')
+        for i in fif:
+            f.write(f'[{time.ctime()}] : {i}\n')
+
+
+        # creating thread
+    for i in range(nm):
+        t = threading.Thread(target=d, args=(i,))
+        print(f'{i} threat created')
+        t.start()
+
+    # both threads completely executed
